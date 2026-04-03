9b141111df74d17a2955dfce5fe4792e3f4eb865
PrestigetheGreat <prestigembungo@gmail.com>
2026-04-03 17:09:01 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const videos = document.querySelectorAll('.video-slide');
+let current = 0;
 
+function switchVideo() {
+  videos[current].classList.remove('active');
+  current = (current + 1) % videos.length;
+  videos[current].classList.add('active');
+  videos[current].currentTime = 0;
+  videos[current].play();
+}
+
+videos.forEach((video, index) => {
+  if (index !== 0) {
+    video.pause();
+  }
+  video.addEventListener('ended', switchVideo);
+});
+
+videos[0].play();
