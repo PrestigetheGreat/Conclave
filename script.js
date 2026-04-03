a0420cbdbf4946fd460f635f2b0dae9205051e16
PrestigetheGreat <prestigembungo@gmail.com>
2026-04-03 23:56:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
 const video = document.getElementById('bg-video');
-const carouselLeft = document.getElementById('carousel-left');
-const carouselRight = document.getElementById('carousel-right');
-const leftWrapper = document.getElementById('carousel-left-wrapper');
-const rightWrapper = document.getElementById('carousel-right-wrapper');
 
 document.addEventListener('DOMContentLoaded', function () {
   video.muted = true;
   video.play().catch(function () {
     console.log('Autoplay blocked');
   });
-  if (carouselLeft) carouselLeft.play();
-  if (carouselRight) carouselRight.play();
 });
 
 window.addEventListener('click', function () {
   video.play();
-  if (carouselLeft) carouselLeft.play();
-  if (carouselRight) carouselRight.play();
 }, { once: true });
-
-let showLeft = true;
-
-if (leftWrapper) leftWrapper.classList.add('visible');
-if (rightWrapper) rightWrapper.classList.remove('visible');
-
-setInterval(function () {
-  if (showLeft) {
-    if (leftWrapper) leftWrapper.classList.remove('visible');
-    setTimeout(function () {
-      if (rightWrapper) rightWrapper.classList.add('visible');
-    }, 400);
-  } else {
-    if (rightWrapper) rightWrapper.classList.remove('visible');
-    setTimeout(function () {
-      if (leftWrapper) leftWrapper.classList.add('visible');
-    }, 400);
-  }
-  showLeft = !showLeft;
-}, 2000);
 
 const ticket = document.querySelector('.floating-ticket');
 let x = 50;
